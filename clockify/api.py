f6fc1208822023bfea324364052f3a38edfe9132
Jordan Amos <jordan.amos@justice.qld.gov.au>
2023-05-10 14:45:17 +1000

    add cookies support?

--- clockify/api.py
+++ clockify/api.py
@@ -6,7 +6,7 @@ import urllib.request
 from datetime import date
 from json.decoder import JSONDecodeError
 from typing import Any
-
+from http import cookiejar
 from requests import Session
 
 
@@ -53,6 +53,8 @@ class ClockifySessionURLLIB:
                 "Connection to Clockify's API requires an API Key which can"
                 "be found in your user settings."
             )
+        self.cookie_jar = cookiejar.CookieJar()
+        self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie_jar))
         self.api_key = api_key
         self.headers = {
             "X-Api-key": api_key,
@@ -63,9 +65,7 @@ class ClockifySessionURLLIB:
         """Performs a "GET" request to the clockify API. Returns the JSON response."""
         url = f"{self.API_BASE_ENDPOINT}/{endpoint}"
         request = urllib.request.Request(url, headers=self.headers)
-        resp = urllib.request.urlopen(request)
-        print(resp)
-        print(vars(resp))
+        resp = self.opener.open(request)
         try:
             return json.load(resp)
         except JSONDecodeError:
